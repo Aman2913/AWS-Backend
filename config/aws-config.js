f46c0b1e5fa65658de405dba42933d6be959fa3d
AMAN KUMAR VERMA <96662657+Aman2913@users.noreply.github.com>
2025-07-13 01:44:44 +0530

    Update aws-config.js

--- config/aws-config.js
+++ config/aws-config.js
@@ -1,11 +1,7 @@
 const AWS = require("aws-sdk");
 
 // Directly setting credentials (not via .env)
-AWS.config.update({
-  region: "eu-north-1",
-  accessKeyId: "AKIASFJ3WPGPKQ2RZQVM",
-  secretAccessKey: "y9XFauN6gM5mpvQp9dViwK705HztepBfYe03JhWS",
-});
+AWS.config.update({region: "eu-north-1"});
 
 const s3 = new AWS.S3();
 const S3_BUCKET = "sampleaman-01bucket";
